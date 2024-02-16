6119b28804eacf27ec1008a8322f809132e5d173
TheEliasBe <elias.berger@tum.de>
2024-02-16 10:17:34 +0100

    improved submit logic

--- frontend/src/components/Form.client.tsx
+++ frontend/src/components/Form.client.tsx
@@ -1,53 +1,93 @@
 "use client";
-import React, { useState } from 'react';
+import React, {useState, useRef} from 'react';
 import ResultText from './ResultText';
 
 const InputForm = () => {
+    const skillsInputRef = useRef<HTMLInputElement>(null);
+    const expertiseInputRef = useRef<HTMLInputElement>(null);
+    const [submitted, setSubmitted] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [idea, setIdea] = useState('');
+    const [description, setDescription] = useState('');
     const [skill, setSkill] = useState('');
     const [expertise, setExpertise] = useState('');
-    const [submitted, setSubmitted] = useState(false);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        // Assuming you process the inputs to generate the 'idea'
-        const idea = "a revolutionary platform"; // Placeholder for the idea generation logic
-        setSubmitted(true); // Update the submitted state to true
-        // Optionally, set the skill and expertise states based on input values
+        setLoading(true); // Start loading animation
+        const skills = skillsInputRef.current?.value;
+        const expertise = expertiseInputRef.current?.value;
+
+        console.log(skills);
+        console.log(expertise);
+
+        // Here, replace 'YOUR_API_ENDPOINT' with your actual FastAPI endpoint URL
+        const response = await fetch('http://127.0.0.1:8000/generate-business-idea/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({skills, expertise}),
+        });
+
+        if (response.ok) {
+            const result = await response.json();
+            setIdea(result.business_idea_name);
+            setDescription(result.business_idea_description);
+            setSkill(result.skill)
+            setExpertise(result.expertise)
+            setSubmitted(true);
+        } else {
+            console.error('Server error:', response.statusText);
+            // Handle server errors or invalid responses here
+        }
+
+        setLoading(false); // End loading animation
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label htmlFor="textbox1" className="block text-sm font-medium text-gray-700">List your skills (comma separated)</label>
-                    <input
-                        type="text"
-                        id="textbox1"
-                        name="textbox1"
-                        onChange={(e) => setSkill(e.target.value)}
-                        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                        placeholder="Online Marketing, Machine Learning, etc."
-                    />
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="textbox2" className="block text-sm font-medium text-gray-700">List your expert areas (comma separated)</label>
-                    <input
-                        type="text"
-                        id="textbox2"
-                        name="textbox2"
-                        onChange={(e) => setExpertise(e.target.value)}
-                        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                        placeholder="Running, Travel, etc."
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="w-full px-4 py-2 bg-indigo-600 text-white font-bold rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
-                >
-                    Submit
-                </button>
+
+                    <div className="mb-4">
+                        <label htmlFor="textbox1" className="block text-sm font-medium text-gray-700">List your skills
+                            (comma
+                            separated)</label>
+                        <input
+                            type="text"
+                            id="textbox1"
+                            name="textbox1"
+                            ref={skillsInputRef}
+                            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                            placeholder="Online Marketing, Machine Learning, etc."
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="textbox2" className="block text-sm font-medium text-gray-700">List your expert
+                            areas
+                            (comma separated)</label>
+                        <input
+                            type="text"
+                            id="textbox2"
+                            name="textbox2"
+                            ref={expertiseInputRef}
+                            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                            placeholder="Running, Travel, etc."
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full px-4 py-2 bg-indigo-600 text-white font-bold rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
+                    >
+                        Submit
+                    </button>
             </form>
-            {submitted && <ResultText skill={skill} expertise={expertise} idea="GreenSense: AI-Driven Plant Health Monitoring"/>}
+            {loading && <div
+                className="flex justify-center mt-4">
+                <span className="loading loading-spinner loading-xs"></span>
+            </div>}
+            {submitted && !loading &&
+                <ResultText skill={skill} expertise={expertise} idea={idea} description={description}/>}
         </div>
     );
 };
--- frontend/src/components/ResultText.tsx
+++ frontend/src/components/ResultText.tsx
@@ -2,28 +2,21 @@ interface DynamicTextProps {
     skill: string;
     expertise: string;
     idea: string;
+    description: string;
 }
 
-const ResultText: React.FC<DynamicTextProps> = ({skill, expertise, idea}) => {
+const ResultText: React.FC<DynamicTextProps> = ({skill, expertise, idea, description}) => {
     return (
         <div className="text-center p-4">
             <p className="text-lg">
                 With your skill in <span className="font-bold">{skill} </span>
                 and your expertise in <span className="font-bold">{expertise}</span>,
                 a business idea is <br/>
-                <span
+                <p
                     className="font-bold text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 mt-4">
-      {idea}
-    </span>
-                <br/>
-                <p className="mt-4">GreenSense offers a revolutionary approach to plant care through an AI-driven
-                    monitoring system. Utilizing advanced machine learning algorithms, it provides real-time analysis of
-                    plant health, detecting early signs of disease, nutrient deficiencies, and water stress. The system
-                    uses sensors to gather data on soil moisture, light levels, temperature, and humidity, alongside
-                    image recognition technology to identify issues through leaf patterns. It offers personalized care
-                    recommendations for each plant, optimizing watering schedules, fertilization, and disease
-                    prevention. Designed for both indoor gardeners and commercial agriculture, GreenSense aims to
-                    enhance plant longevity, boost yields, and simplify the complexities of plant care.</p>
+                    {idea}
+                </p>
+                <p className="mt-4">{description}</p>
             </p>
         </div>
     );
